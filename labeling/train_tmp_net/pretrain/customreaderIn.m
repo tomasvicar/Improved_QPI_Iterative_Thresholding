3f70be5232ff34bb0c64e5d0fa654d148318271b
tomasvicar <tomasvicar@gmail.com>
2020-07-01 09:03:25 +0200

    remove labeling

--- labeling/train_tmp_net/pretrain/customreaderIn.m
+++
@@ -1,5 +0,0 @@
-function data = customreaderIn(name)
-    global max_val min_val
-
-    data=single(mat2gray(double(imread(name)),double([min_val max_val]))-0.5);
-end
--- labeling/utils/hysteresis_thresh.m
+++
@@ -1,8 +0,0 @@
-function final = hysteresis_thresh(image,tLo,tHi)
-
-highmask = image>tHi;
-lowmask = bwlabel(~(image<tLo));
-final = ismember(lowmask,unique(lowmask(highmask)));
-
-end
-
